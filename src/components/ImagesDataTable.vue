fe45f0e4fe19eb306b9c5b133c64ec67a43e009d
Egor Ivanov <rust_lang_lover@proton.me>
2024-09-26 23:07:53 +0300

    // added delete button in table for deleting files

--- src/components/ImagesDataTable.vue
+++ src/components/ImagesDataTable.vue
@@ -3,8 +3,10 @@ import { ref } from 'vue';
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import extract_file_format from "@/structs/tool_functions/extract_file_format.ts";
+import TableDeleteButton from "@/components/TableDeleteButton.vue";
 function fetchImages() : string[] {
-  return ["https://trustedapi.space/images/picture1.webp",
+  return [
+    "https://trustedapi.space/images/picture1.webp",
     "https://trustedapi.space/images/picture2.webp",
     "https://trustedapi.space/images/picture3.webp",
     "https://trustedapi.space/images/picture4.webp"
@@ -28,7 +30,11 @@ const imageData = ref(
       <Column field="name" header="Ссылка"></Column>
       <Column field="category" header="Тип данных"></Column>
       <Column field="format" header="Формат"></Column>
-      <Column header="Удалить"></Column>
+      <Column header="Удалить">
+        <template #body="slotProps">
+          <TableDeleteButton :link-to-delete="slotProps.data.name"/>
+        </template>
+      </Column>
     </DataTable>
   </div>
 </template>
---
+++ src/components/TableDeleteButton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Button from "primevue/button";
+const props = defineProps<{
+  linkToDelete : string
+}>();
+function deleteImageFromBase() {
+  // There should be placed a logic for deleting the image from the db
+  console.log(props.linkToDelete)
+}
+</script>
+
+<template>
+<Button
+    icon="pi pi-eraser"
+    severity="danger"
+    @click="deleteImageFromBase"
+/>
+</template>
+
+<style scoped>
+
+</style>
